550dbcdd035b49ab84ce43b402d9e45c2360df3c
Nicholas Tan <swaggychoz@gmail.com>
2024-07-03 15:54:57 +0800

    commit exercise 12.20

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+console.log(import.meta.env.VITE_BACKEND_URL)
+console.log(import.meta.env.VITE_BACKEND_URL)
+console.log(import.meta.env.VITE_BACKEND_URL)
+console.log(import.meta.env.VITE_BACKEND_URL)
+console.log(import.meta.env.VITE_BACKEND_URL)
+
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
 })
